f4e7f27c8d38a89cba24d179ed9a6f3b89a4a51e
Karandeep Singh <kasingh@f1br7s8.clusters.42paris.fr>
2025-06-29 13:43:02 +0200

    update front ajout d'un zoom plus changement de color sur le canvas

--- src/front/src/templates/pongTemplate.ts
+++ src/front/src/templates/pongTemplate.ts
@@ -1,17 +1,27 @@
 export const pongTemplate = `
-<div>
-    <div id="scoreboard" class="mb-4 text-black text-2xl text-center font-arcade">
-      <span id="scoreText">0 - 0</span>
+<div class="flex flex-col items-center">
+
+  <!-- Conteneur scoreboard + labels, même largeur que le canvas (600px) -->
+  <div class="flex items-center justify-between w-[600px] mb-4">
+    <span id="player1Label" class="font-arcade text-accent">Player 1</span>
+    <div id="scoreboard" class="text-white text-2xl font-arcade">
+      <span id="scoreText">0 – 0</span>
     </div>
-    <canvas
-      id="pongCanvas"
-      width="600"
-      height="400"
-      class="border-2 border-accent block mx-auto mb-4"
-    ></canvas>
-    <button
-      id="backBtn"
-      class="threeD-button-set"
-    >Back</button>
+    <span id="player2Label" class="font-arcade text-accent">Player 2</span>
+  </div>
+
+  <!-- Canvas central -->
+  <canvas
+    id="pongCanvas"
+    width="600"
+    height="400"
+    class="block bg-black/70"
+  ></canvas>
+
+  <!-- Bouton Back -->
+  <button
+    id="backBtn"
+    class="threeD-button-set mt-4"
+  >Back</button>
 </div>
 `
